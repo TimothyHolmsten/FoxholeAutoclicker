135a6562898221228304289ef3c56cc7614c1fe8
TimothyHolmsten <timothy.holmsten@gmail.com>
2024-08-07 20:43:27 +0200

    still bugs but works better

--- src/clicker.rs
+++ src/clicker.rs
@@ -2,24 +2,33 @@ use enigo::{Direction::{Press, Release, Click}, Enigo, Mouse, Settings, Button::
 
 pub struct Clicker {
     pub enigo: Enigo,
+    pub holding: bool
 }
 
 impl Clicker {
     pub fn new() -> Self {
         Clicker {
             enigo: {let enigo = Enigo::new(&Settings::default()).unwrap(); enigo},
+            holding: false
         }
     }
 
     pub fn click(&mut self) {
         self.enigo.button(Left, Click).expect("Could not click");
+        self.holding = false;
     }
 
     pub fn hold_down(&mut self) {
-        self.enigo.button(Left, Press).expect("Could not hold down");
+        if !self.holding {
+            self.enigo.button(Left, Press).expect("Could not hold down");
+            self.holding = true;
+        }
     }
 
     pub fn release(&mut self) {
-        self.enigo.button(Left, Release).expect("Could not release");
+        if self.holding {
+            self.enigo.button(Left, Release).expect("Could not release");
+            self.holding = false;
+        }
     }
 }
