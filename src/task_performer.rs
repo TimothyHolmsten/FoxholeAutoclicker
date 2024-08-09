9b97fa786b672d3c1901a61eb05ba5277020183e
TimothyHolmsten <timothy.holmsten@gmail.com>
2024-08-09 17:15:50 +0200

    Performance fixes

--- src/task_performer.rs
+++ src/task_performer.rs
@@ -1,6 +1,7 @@
 use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
 use std::time::Duration;
+
 use crate::clicker::Clicker;
 use crate::event_listener::Command;
 use crate::tasks::task::Task;
@@ -36,12 +37,13 @@ impl TaskPerformer {
                 // Receive a command from the channel
                 let command = {
                     let receiver = receiver.lock().unwrap();
-                    receiver.recv_timeout(Duration::from_millis(10))
+                    receiver.recv_timeout(Duration::from_millis(50)) // Adjusted timeout
                 };
 
                 match command {
                     Ok(command) => {
                         let mut task_lock = current_task.lock().unwrap();
+                        let mut macro_lock = macro_positions.lock().unwrap(); // Lock macro_positions here
 
                         match command {
                             Command::StartClicking => {
@@ -61,16 +63,16 @@ impl TaskPerformer {
                             }
                             Command::StopClicking => {
                                 *task_lock = None;
-                            },
+                            }
                             Command::None => *task_lock = None,
                             Command::ClearMacro => {
-                                let mut positions = macro_positions.lock().unwrap();
-                                positions.clear();
+                                macro_lock.clear(); // Clear macro_positions here
                                 *task_lock = None;
-                            },
+                            }
                         }
                     }
                     Err(mpsc::RecvTimeoutError::Timeout) => {
+                        // Minor delay to reduce CPU usage
                         thread::sleep(Duration::from_millis(10));
                     }
                     Err(mpsc::RecvTimeoutError::Disconnected) => {
